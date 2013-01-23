3b0d949b4b2a8de795f8e87bdb44ce0c8a761963
Karl Lattimer <karl@qdh.org.uk>
2013-01-23 10:19:31 +0000

    missing includes

--- src/pifan.c
+++ src/pifan.c
@@ -9,9 +9,10 @@
 #include <sys/un.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define TRUE 1
-#define FALSE 0
+//#define TRUE 1
+//#define FALSE 0
 
 void usage(char *error, char *binaryPath) {
     if (error) {
